a4ee960f1a4e834a83dffbc3bc603ba615f7ccac
Quentin-Fontaine <quentin.font974@gmail.com>
2020-02-13 14:01:58 +0100

    Site
    La première version du site (carte, description original) !

---
+++ head.php
@@ -0,0 +1,13 @@
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<!-- Leaflet : map -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
+<meta charset="utf-8">
+<title>Scrapping Reddit</title>
+
+<script type='text/javascript' src='jquery-3.4.1.min.js'></script>
+<link rel='stylesheet' type='text/css' href='jquery-ui-1.12.1/jquery-ui.min.css'>
