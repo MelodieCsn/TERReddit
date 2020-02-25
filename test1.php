39b319c27b1d446f1de357a675ac262835631b8e
Saleh DASSOUKHI <57576759+dassoukhi@users.noreply.github.com>
2020-02-25 20:39:01 +0100

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  </head>
+  <body>
+
+
+
+<script>
+   
+
+    function affiche(objet)
+   {
+    console.log(objet.afterClean);
+   }
+
+
+    $().ready(function(){
+
+       $.getJSON("fic.json",function(data)
+     {
+      $.each(data,function(i,objet)
+      {
+        $("body").append("<a href='"+objet.url+"' onmouseenter=\"affiche('"+objet.afterClean+"')\" ><img src='"+objet.url+"' alt='Smiley face' height='200' width='200'/></a>")
+
+      })
+     })
+    })
+
+
+   </script>
+
+
+
+  </body>
+</html>
+
+
